df648bf6f6d233caef1c9cdf0b6d10f7baeb912b
JadenKim-dev <jaden.kim5179@gmail.com>
2023-03-22 23:48:34 +0900

    refactor(config): typeorm config option 생성 factory 함수를 별도의 파일로 분리

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,14 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { createTypeOrmOptions } from './config/typeorm.config';
 
 @Module({
   imports: [
     ConfigModule,
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get('DATABASE_HOST'),
-        port: 3306,
-        username: configService.get('DATABASE_USER'),
-        password: configService.get('DATABASE_PASSWORD'),
-        database: configService.get('DATABASE_NAME'),
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
+      useFactory: createTypeOrmOptions,
       inject: [ConfigService],
     }),
   ],
