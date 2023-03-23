4a8436fc82adce3a21c9d695027ab01cab0f9588
JadenKim-dev <jaden.kim5179@gmail.com>
2023-03-23 22:51:01 +0900

    feat(logger): 모든 http 요청에 대해서 pino logger 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
+import { LoggerModule } from 'nestjs-pino';
+
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { createTypeOrmOptions } from './config/typeorm.config';
+import { loggerOptions } from './config/logger.config';
 
 @Module({
   imports: [
+    LoggerModule.forRoot(loggerOptions),
     ConfigModule,
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
