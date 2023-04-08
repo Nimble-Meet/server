6f87ca684494cc2aa03e090d9358fab0ac13441e
JadenKim-dev <jaden.kim5179@gmail.com>
2023-04-08 15:09:52 +0900

    test(auth): EncryptedPassword에 대한 테스트 코드 작성

---
+++ src/auth/EncryptedPassword.spec.ts
@@ -0,0 +1,31 @@
+import { EncryptedPassword } from './EncryptedPassword';
+
+describe('EncryptedPassword', () => {
+  const plainPassword = 'password123';
+  let encryptedPassword: EncryptedPassword;
+
+  beforeEach(async () => {
+    encryptedPassword = await EncryptedPassword.encryptFrom(plainPassword);
+  });
+
+  it('encryptFrom: 원본 비밀번호가 암호화된 상태로 생성되어야 함', () => {
+    expect(encryptedPassword.valueOf()).not.toEqual(plainPassword);
+  });
+
+  it('from: 암호화된 비밀번호로부터 생성', () => {
+    const hashedPassword = encryptedPassword.valueOf();
+    const result = EncryptedPassword.from(hashedPassword);
+    expect(result).toEqual(encryptedPassword);
+  });
+
+  it('equals: 적절한 원본 비밀번호가 주어지면 true', () => {
+    const result = encryptedPassword.equals(plainPassword);
+    expect(result).toBe(true);
+  });
+
+  it('equals: 일치하지 않는 원본 비밀번호가 주어지면 false', () => {
+    const incorrectPassword = 'invalid-password123';
+    const result = encryptedPassword.equals(incorrectPassword);
+    expect(result).toBe(false);
+  });
+});
