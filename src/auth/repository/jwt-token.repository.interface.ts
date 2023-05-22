1195706ce7b59a38858a264b6cc8cc48b1ecb690
JadenKim-dev <jaden.kim5179@gmail.com>
2023-05-22 20:56:55 +0900

    refactor(JwtToken): JwtRepository 에서 필요한 부분에 nullable 적용
    - TypeORM의 기본 Repository의 경우 findOne이 nullable한 값을 반환하므로, 이에 맞게 JwtTokenRepository 메서드들의 타입을 수정

--- src/auth/repository/jwt-token.repository.interface.ts
+++ src/auth/repository/jwt-token.repository.interface.ts
@@ -1,8 +1,8 @@
 import { JwtToken } from '../entity/jwt-token.entity';
 
 export interface IJwtTokenRepository {
-  findOneByRefreshToken(refreshToken: string): Promise<JwtToken>;
-  findTokenIdByUserId(userId: number): Promise<number>;
+  findOneByRefreshToken(refreshToken: string): Promise<JwtToken | null>;
+  findTokenIdByUserId(userId: number): Promise<number | null>;
   save(jwtToken: JwtToken): Promise<JwtToken>;
 }
 
