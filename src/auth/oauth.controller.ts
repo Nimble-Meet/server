103c1bcdc2b783463ea234f276e43a2e39e4a213
JadenKim-dev <jaden.kim5179@gmail.com>
2023-06-03 21:09:42 +0900

    docs(auth): 구글 로그인 관련 swagger docs 작성

--- src/auth/oauth.controller.ts
+++ src/auth/oauth.controller.ts
@@ -11,12 +11,20 @@ import {
   Inject,
   UseInterceptors,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiFoundResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { IAuthService } from './auth.service.interface';
 import { GoogleAuthGuard } from './guards/google-auth.guard';
 import { OauthPayloadDto } from './dto/oauth-payload.dto';
+import { LoginResponseDto } from './dto/response/login-response.dto';
+import { GoogleLoginUnauthorizedResponseDto } from './dto/error/google-login-unauthorized-response.dto';
 
-@ApiTags('auth')
+@ApiTags('oauth')
 @Controller('api/auth/login')
 export class OauthController {
   constructor(
@@ -25,11 +33,27 @@ export class OauthController {
   ) {}
 
   @Get('google')
+  @ApiOperation({ description: '구글 로그인/회원가입' })
+  @ApiFoundResponse({
+    description: '구글 계정 인증 페이지 이동',
+  })
   @UseGuards(GoogleAuthGuard)
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   async googleLogin(): Promise<void> {}
 
   @Get('google/callback')
+  @ApiOperation({ description: '구글 로그인/회원가입 콜백 URL' })
+  @ApiFoundResponse({
+    description: '구글 oauth 정보 기반 로그인/회원가입 처리',
+  })
+  @ApiCreatedResponse({
+    description: '구글 로그인/회원가입 성공',
+    type: LoginResponseDto,
+  })
+  @ApiUnauthorizedResponse({
+    description: '다른 로그인 방식으로 가입한 이메일',
+    type: GoogleLoginUnauthorizedResponseDto,
+  })
   @UseGuards(GoogleAuthGuard)
   @UseInterceptors(SetRTCookieInterceptor)
   async googleLoginCallback(
